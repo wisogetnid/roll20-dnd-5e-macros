cf2f1bc4d92b7990c29e9797e2f67a6900f83744
tobi vogel <wisogetnid@gmail.com>
2020-06-20 10:08:38 +0700

    added invocation arguments support for quicker use

--- generate_spellcard-slots.sh
+++ generate_spellcard-slots.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo Generate spellcard entry. \n For which level [cantrip, 1 - 9]?
-read level
-echo For which slot [0 - 9]
-read slot
+level=$1
+slot=$2
+
+if [ -z "$level" ]
+  then
+    echo For which level [cantrip, 1 - 9]?
+    read level
+fi
+
+if [ -z "$slot" ]
+  then
+    echo For which slot [0 - 9]
+    read slot
+fi
 
 echo ----------- copy below ----------------
 sed "s/<<level>>/$level/g" spells/spellcard-slots.txt | sed "s/<<slot>>/$slot/g"
