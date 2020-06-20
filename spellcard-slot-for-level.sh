c778b0ca6e4daab87faeb6623ed25b0254de9af2
tobi vogel <wisogetnid@gmail.com>
2020-06-20 09:32:43 +0700

    create script for easier macro generation

---
+++ spellcard-slot-for-level.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Generate spellcard entry. \n For which level [cantrip, 1 - 9]?
+read level
+echo For which slot [0 - 9]
+read slot
+
+echo ">generate for< level $level and slot \$ $slot"
