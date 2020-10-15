63d6bf75c11418281519e7d941f30a9d58995a50
tobi vogel <wisogetnid@gmail.com>
2020-10-15 19:23:14 +0100

    another rename

--- src/monster-template/generate_documentation.sh
+++ src/monster-template/generate_documentation.sh
@@ -3,7 +3,7 @@
 CD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 OUTPUT_FOLDER="documentation/monster"
-OUTPUT_FILE=doc_monster-abilities.md
+OUTPUT_FILE=doc_monster-template-abilities.md
 OUTPUT_PATH="./$OUTPUT_FOLDER/$OUTPUT_FILE"
 
 cat $CD/traits-ability-explanation.md >> $OUTPUT_FILE
