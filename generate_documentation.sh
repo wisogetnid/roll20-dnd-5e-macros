e42d2be66f9b4706296958e2ea7daf6efd3ed4ef
tobi vogel <wisogetnid@gmail.com>
2020-10-15 19:22:01 +0100

    rename monster documentation, delete doc before (re-)generating

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf ./documentation
+echo "deleted existing documentation"
+
 ./src/character/generate_documentation.sh
 ./src/monster-template/generate_documentation.sh
 
@@ -7,3 +10,5 @@ for level in {1..9}
 do
  ./src/spells/generate_documentation.sh $level
 done
+
+echo "generated full documentation"
--- src/monster-template/generate_documentation.sh
+++ src/monster-template/generate_documentation.sh
@@ -2,8 +2,8 @@
 
 CD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-OUTPUT_FOLDER="documentation/monster-template"
-OUTPUT_FILE=doc_monster-traits.md
+OUTPUT_FOLDER="documentation/monster"
+OUTPUT_FILE=doc_monster-abilities.md
 OUTPUT_PATH="./$OUTPUT_FOLDER/$OUTPUT_FILE"
 
 cat $CD/traits-ability-explanation.md >> $OUTPUT_FILE
